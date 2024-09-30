f0b8fb6d62ce2a0aa3c27e2b8619caabdce27a3a
Sandra <sandra.valaviciute@gmail.com>
2024-09-30 17:00:16 +0300

    service filtering tested, specification corrected for filtering with range (books with no ratings treated as 0)

--- src/main/java/com/sv/book_search/service/BookSpecifications.java
+++ src/main/java/com/sv/book_search/service/BookSpecifications.java
@@ -1,9 +1,14 @@
 package com.sv.book_search.service;
 
 import com.sv.book_search.entity.Book;
+import com.sv.book_search.entity.Rating;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,6 +25,54 @@ public class BookSpecifications {
             }
 
             return predicate;
+        };
+    }
+
+    public static Specification<Book> filterBooks(
+            String title, Integer minRating, Integer maxRating, Integer year, String author, String genre, List<String> keywords) {
+
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            // Add filters based on provided parameters
+            if (title != null && !title.isEmpty()) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("title")), "%" + title.toLowerCase() + "%"));
+            }
+
+            if (year != null) {
+                predicates.add(criteriaBuilder.equal(root.get("publicationYear"), year));
+            }
+
+            if (author != null && !author.isEmpty()) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("author")), "%" + author.toLowerCase() + "%"));
+            }
+
+            if (genre != null && !genre.isEmpty()) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("genre")), "%" + genre.toLowerCase() + "%"));
+            }
+
+
+            if (minRating != null || maxRating != null) {
+                Join<Book, Rating> ratingsJoin = root.join("ratings", JoinType.LEFT);
+                query.groupBy(root.get("id")); // Group by book ID to calculate the average rating
+
+                Expression<Double> avgRating = criteriaBuilder.coalesce(criteriaBuilder.avg(ratingsJoin.get("rating")), 0.0);
+                double min = minRating != null ? minRating.doubleValue() : 0.0;
+                double max = maxRating != null ? maxRating.doubleValue() : 5.0;
+
+                query.having(criteriaBuilder.between(avgRating, min, max));
+            }
+
+
+            if (keywords != null && !keywords.isEmpty()) {
+                List<Predicate> keywordPredicates = new ArrayList<>();
+                for (String keyword : keywords) {
+                    keywordPredicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("description")), "%" + keyword.toLowerCase() + "%"));
+                }
+                predicates.add(criteriaBuilder.and(keywordPredicates.toArray(new Predicate[0])));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
 }
--- src/test/java/com/sv/book_search/service/BookServiceTest.java
+++ src/test/java/com/sv/book_search/service/BookServiceTest.java
@@ -6,25 +6,22 @@ import com.sv.book_search.entity.Rating;
 import com.sv.book_search.repository.BookRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.jpa.domain.Specification;
 
-import com.sv.book_search.service.BookSpecifications;
-
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,10 +30,7 @@ public class BookServiceTest {
     private BookRepository bookRepository;
 
     private List<Book> books;
-    private List<BookWithAverageRatingDTO> expectedBooks;
-
-    @InjectMocks
-    private  RatingService ratingService;
+    private List<BookWithAverageRatingDTO> allBooks;
 
     @InjectMocks
     private BookService bookService;
@@ -48,33 +42,34 @@ public class BookServiceTest {
         List<Rating> ratings;
 
         Book book1 = Book.builder()
+                .title("Book 1")
                 .id(1L)
+                .author("Petras Ma P")
                 .publicationYear(2008)
+                .description("kazkoki aprasyma")
                 .genre("Technology")
-                .author("Brian Joshua Bloch")
-                .title("Effective Java")
-                .description("A comprehensive guide to Java")
                 .build();
 
         Book book2 = Book.builder()
+                .title("Book 2")
                 .id(2L)
-                .publicationYear(2024)
-                .genre("Technology")
-                .author("Brian Goetz")
-                .title("Java Concurrency in Practice")
-                .description("A book on Java concurrency")
+                .author("Roberta Ma")
+                .publicationYear(2021)
+                .description("tingiu rasyt aprasyma")
+                .genre("Fiction")
                 .build();
 
         Book book3 = Book.builder()
+                .title("Book 3")
                 .id(3L)
-                .publicationYear(2011)
-                .title("Steve Jobs")
-                .description("The biography of Apple co-founder Steve Jobs a")
-                .author("Walter Isaacson")
-                .genre("Biography")
+                .author("Andrius Jas")
+                .publicationYear(2024)
+                .description("daug visokiu zodziu")
+                .genre("Psychology")
                 .build();
 
         books = Arrays.asList(book1, book2, book3);
+        bookRepository.saveAll(books);
 
         Rating rating1 = Rating.builder()
                 .rating(5)
@@ -89,137 +84,69 @@ public class BookServiceTest {
                 .build();
 
         Rating rating3 = Rating.builder()
-                .rating(2)
-                .book(book2)
+                .rating(5)
+                .book(book3)
                 .username("user2@example.com")
                 .build();
 
         ratings = Arrays.asList(rating1, rating2);
 
         book1.setRatings(ratings);
-        book2.setRatings(Collections.singletonList(rating3));
+        book3.setRatings(Collections.singletonList(rating3));
 
-        expectedBooks = Arrays.asList(
-                new BookWithAverageRatingDTO(1L, "Effective Java", "Brian Joshua Bloch", 2008, "A comprehensive guide" +
-                        " to Java", "Technology",
-                        4.0),
-                new BookWithAverageRatingDTO(2L, "Java Concurrency in Practice", "Brian Goetz", 2024, "A book on Java concurrency", "Technology",
-                        2.0),
-                new BookWithAverageRatingDTO(3L, "Steve Jobs", "Walter Isaacson", 2011, "The biography of Apple " +
-                        "co-founder Steve Jobs a",
-                        "Biography", 0.0)
+        allBooks = Arrays.asList(
+                new BookWithAverageRatingDTO(1L, "Book 1", "Petras Ma P", 2008, "kazkoki aprasyma", "Technology", 4.0),
+                new BookWithAverageRatingDTO(2L, "Book 2", "Roberta Ma", 2021, "tingiu rasyt aprasyma",
+                        "Fiction",
+                        0.0),
+                new BookWithAverageRatingDTO(3L, "Book 3", "Andrius Jas", 2024, "daug visokiu zodziu",
+                        "Psychology", 5.0)
         );
 
     }
 
-    @Test
-    public void testGetAllBooks() {
-        when(bookRepository.findAll()).thenReturn(books);
-        List<BookWithAverageRatingDTO> actualBooks = bookService.getAllBooks();
-
-        Assertions.assertThat(actualBooks).isEqualTo(expectedBooks);
+    private static Stream<Arguments> provideFilterArguments() {
+        return Stream.of(
+                // keywords
+                Arguments.of(null, null, null, null, null, null, Arrays.asList("aprasyma", "kazkoki"), Arrays.asList(0)),
+                // year
+                Arguments.of(null, null, null, 2008, null, null, null, Arrays.asList(0)),
+                // author
+                Arguments.of(null, null, null, null, "ma", null, null, Arrays.asList(0, 1)),
+                // genre
+                Arguments.of(null, null, null, null, null, "technology", null, Arrays.asList(0)),
+                // title
+                Arguments.of("book 1", null, null, null, null, null, null, Arrays.asList(0)),
+                // rating range
+                Arguments.of(null, 3, 4, null, null, null, null, Arrays.asList(0)),
+                // Combined filter
+                Arguments.of("book", 4, 5, 2024, "andrius", "psychology", Arrays.asList("daug"), Arrays.asList(2)),
+                // all
+                Arguments.of(null, null, null, null, null, null, null, Arrays.asList(0, 1, 2))
+        );
     }
 
-    @Test
-    public void testGetBooksByTitle() {
-
-        String title = "Java";
-
-        when(bookRepository.findByTitleContainingIgnoreCase(title)).thenReturn(Arrays.asList(books.get(0),
-                books.get(1)));
-
-        List<BookWithAverageRatingDTO> actualBooks = bookService.getBooksByTitle(title);
-
-        Assertions.assertThat(actualBooks).isEqualTo(Arrays.asList(expectedBooks.get(0), expectedBooks.get(1)));
-        Assertions.assertThat(actualBooks.get(0).getAverageRating()).isEqualTo(expectedBooks.get(0).getAverageRating());
-        Assertions.assertThat(actualBooks.get(0).getTitle()).isEqualTo(expectedBooks.get(0).getTitle());
-        Assertions.assertThat(actualBooks.get(0).getId()).isEqualTo(expectedBooks.get(0).getId());
-    }
-
-    @Test
-    public void testGetBooksByAuthor() {
-
-        String author = "brian";
-
-        when(bookRepository.findByAuthorContainingIgnoreCase(author)).thenReturn(Arrays.asList(books.get(0),
-                books.get(1)));
-
-        List<BookWithAverageRatingDTO> actualBooks = bookService.getBooksByAuthor(author);
-
-        Assertions.assertThat(actualBooks).isEqualTo(Arrays.asList(expectedBooks.get(0), expectedBooks.get(1)));
-        Assertions.assertThat(actualBooks).hasSize(Arrays.asList(expectedBooks.get(0), expectedBooks.get(1)).size());
-    }
-
-    @Test
-    public void testGetBooksByGenre() {
-
-        String genre = "biography";
-
-        when(bookRepository.findByGenreIgnoreCase(genre)).thenReturn(Arrays.asList(books.get(2)));
-
-        List<BookWithAverageRatingDTO> actualBooks = bookService.getBooksByGenre(genre);
-
-        Assertions.assertThat(actualBooks).isEqualTo(Arrays.asList(expectedBooks.get(2)));
-        Assertions.assertThat(actualBooks).hasSize(Arrays.asList(expectedBooks.get(2)).size());
-    }
-
-    @Test
-    public void testGetBooksByDescriptionMultipleKeywords() {
-
-        List<String> keywords = Arrays.asList("java", "a");
+    @ParameterizedTest
+    @MethodSource("provideFilterArguments")
+    public void testFindWithDifferentFilters(String title, Integer minRating, Integer maxRating, Integer year,
+                                             String author, String genre, List<String> keywords,
+                                             List<Integer> booksToGet) {
 
         when(bookRepository.findAll(any(Specification.class)))
-                .thenReturn(Arrays.asList(books.get(0), books.get(1)));
+                .thenReturn(booksToGet.stream()
+                        .map(index -> books.get(index))
+                        .toList());
 
-        List<BookWithAverageRatingDTO> actualBooks = bookService.getBooksByWordsInDescription(keywords);
+        List<BookWithAverageRatingDTO> expectedBooks = booksToGet.stream()
+                .map(index -> allBooks.get(index))
+                .toList();
 
-        Assertions.assertThat(actualBooks).isEqualTo(Arrays.asList(expectedBooks.get(0), expectedBooks.get(1)));
-    }
-
-    @Test
-    public void testGetBooksByDescriptionOneKeyword() {
-
-        List<String> keywords = Arrays.asList("a");
-
-        when(bookRepository.findAll(any(Specification.class)))
-                .thenReturn(books);
-
-        List<BookWithAverageRatingDTO> actualBooks = bookService.getBooksByWordsInDescription(keywords);
-
-        Assertions.assertThat(actualBooks).isEqualTo(expectedBooks);
-    }
-
-    @Test
-    public void testGetBooksByYear() {
-
-        Integer year = 2024;
-
-        when(bookRepository.findByPublicationYear(year)).thenReturn(Arrays.asList(books.get(1)));
-
-        List<BookWithAverageRatingDTO> actualBooks = bookService.getBooksByYear(year);
-
-        Assertions.assertThat(actualBooks).isEqualTo(Arrays.asList(expectedBooks.get(1)));
-        Assertions.assertThat(actualBooks.get(0).getPublicationYear()).isEqualTo(expectedBooks.get(1).getPublicationYear());
-        Assertions.assertThat(actualBooks.get(0).getId()).isEqualTo(expectedBooks.get(1).getId());
-    }
-
-    @Test
-    public void testGetBooksByRange() {
-
-        double min = 3.0;
-        double max = 5.0;
+        List<BookWithAverageRatingDTO> foundBooks = bookService.filterBooks(title, minRating, maxRating, year, author, genre, keywords);
 
 
-        when(bookRepository.findAll()).thenReturn(books);
 
-        List<BookWithAverageRatingDTO> actualBooks = bookService.getBooksByRating(Optional.of(min), Optional.of(max));
-        Assertions.assertThat(actualBooks).isEqualTo(Arrays.asList(expectedBooks.get(0)));
 
-        List<BookWithAverageRatingDTO> noBooks = bookService.getBooksByRating(Optional.of(max), Optional.of(max));
-        Assertions.assertThat(noBooks).isEqualTo(Arrays.asList());
-
-        List<BookWithAverageRatingDTO> allBooks = bookService.getBooksByRating(Optional.empty(), Optional.of(max));
-        Assertions.assertThat(allBooks).isEqualTo(expectedBooks);
+        Assertions.assertThat(foundBooks).containsExactlyInAnyOrderElementsOf(expectedBooks);
     }
 
 }
